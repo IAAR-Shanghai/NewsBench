e4757b6d56926f124c516cbec2fe0828bb887918
wpy <1097266341@qq.com>
2024-03-07 22:25:34 +0800

    修改路径名称

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '/news_benchmark/'
+  publicPath: '/NewsBench/'
 })
